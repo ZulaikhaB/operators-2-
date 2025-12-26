7cc4f6c29a34d7c27316bcb8468cf823a7cbbd67
ZulaikhaB <bestwishes88@hotmail.com>
2025-12-26 11:38:16 +0000

    Add files via upload

---
+++ operators 3.py
@@ -0,0 +1,16 @@
+Y = 7
+X = 8
+if (Y is X ):
+    print ("they are the same variable")
+else : 
+    print("they are not")
+
+if (Y is not X ) :
+    print("they are not the same variable")
+
+U = 9
+G = 5
+print(U & G )
+print(U | G)
+print (~ G  )
+print(U << G )
